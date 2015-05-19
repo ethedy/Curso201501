409e360327cb600bf04e320165b5e41448b8aaa9
ethedy <ethedy@gmail.com>
2015-05-19 08:25:38 -0300

    New ProyectoLogin

---
+++ ProyectoLogin/Servicios/Sesion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace Servicios
+{
+  public class Sesion
+  {
+    public Usuario Usuario { get; private set; }
+
+    public string FullName
+    {
+      get { return string.Format("{0} {1}", Usuario.Persona.Nombre, Usuario.Persona.Apellido); }
+    }
+
+    public DateTime PasswordExpira
+    {
+      get
+      {
+        return Usuario.FechaExpiracionPassword;
+      }
+    }
+
+    public Sesion(Usuario connected)
+    {
+      Usuario = connected;
+    }
+  }
+}
---
+++ ProyectoLogin/Terminal/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Servicios;
+
+namespace Terminal
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string uid, pass;
+
+      Console.WriteLine("Bienvenido a la consola de acceso a OMB. Por favor ingrese sus credenciales...");
+      Console.Write("Usuario -->  ");
+      uid = Console.ReadLine();
+
+      Console.Write("Password -->  ");
+      Console.ForegroundColor = ConsoleColor.Black;
+      pass = Console.ReadLine();
+
+      Console.ResetColor();
+
+      //  intentamos acceder al servicio de login...
+      SecurityServices sec = new SecurityServices();
+
+      Sesion sesion = sec.LoginUser(uid, pass);
+      if (sesion == null)
+        Console.WriteLine("Error de credenciales!!");
+      else
+      {
+        Console.WriteLine("Hola {0} su password expira el {1}", 
+          sesion.FullName,
+          sesion.PasswordExpira);
+      }
+      Console.WriteLine("Gracias!!");
+      Console.ReadLine();
+    }
+  }
+}
